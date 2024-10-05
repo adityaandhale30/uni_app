01ee4e81f11859ddea68e7fe95690351c30ecc0b
AdiByte30 <adityaandhale30@gmail.com>
2024-10-05 15:17:19 +0530

    CircleAvatar Widget learned

--- lib/Views/bottomnavigationbar_custom.dart
+++ lib/Views/bottomnavigationbar_custom.dart
@@ -7,58 +7,55 @@ class BottomnavigationbarCustom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  PersistentTabView(
-        context,
-        navBarHeight: 70,
-        navBarStyle: NavBarStyle.style15,
-        screens: [
-          const HomeScreen(),
-          Container(),
-          Container(),
-          Container(),
-          Container()
-        ],
-        items: [
-          PersistentBottomNavBarItem(
-            icon: const Icon(
-              Icons.home_outlined,
-              size: 36,
+    return PersistentTabView(
+      context,
+      navBarHeight: 70,
+      navBarStyle: NavBarStyle.style15,
+      screens: [
+        const HomeScreen(),
+        Container(),
+        Container(),
+        Container(),
+        Container()
+      ],
+      items: [
+        PersistentBottomNavBarItem(
+          icon: const Icon(
+            Icons.home_outlined,
+            size: 36,
+          ),
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.book_outlined),
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(
+            Icons.search_rounded,
+            color: Colors.black,
+          ),
+          inactiveIcon: const Icon(
+            Icons.view_agenda,
+            color: Colors.white,
+          ),
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.fort_outlined),
+        ),
+        PersistentBottomNavBarItem(
+          icon: CircleAvatar(
+            backgroundColor: Colors.grey,
+            foregroundImage: AssetImage("assets/splash.png"),
+            backgroundImage: AssetImage(
+              "assets/harli.jpg",
             ),
           ),
-          PersistentBottomNavBarItem(
-            icon: const Icon(Icons.book_outlined),
-          ),
-          PersistentBottomNavBarItem(
-            icon: const Icon(
-              Icons.search_rounded,
-              color: Colors.black,
-            ),
-            inactiveIcon: const Icon(
-              Icons.view_agenda,
-              color: Colors.white,
+          inactiveIcon: const CircleAvatar(
+            backgroundImage: AssetImage(
+              "assets/harli.jpg",
             ),
           ),
-          PersistentBottomNavBarItem(
-            icon: const Icon(Icons.fort_outlined),
-          ),
-          PersistentBottomNavBarItem(
-            icon: ClipRRect(
-              borderRadius: BorderRadius.circular(80),
-              child: Container(
-                height: 42,
-                width: 42,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.black,
-                ),
-                child: Image.asset(
-                  "assets/harli.jpg",
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-          ),
-        ],
-      );
+        ),
+      ],
+    );
   }
 }
