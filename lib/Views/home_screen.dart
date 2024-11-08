c45da00dfb8a5d31a96aec92cd528b5e1bf5cc73
AdiByte30 <adityaandhale30@gmail.com>
2024-11-08 14:12:46 +0530

    section container created

--- lib/Views/home_screen.dart
+++ lib/Views/home_screen.dart
@@ -6,18 +6,30 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
-        title: const Text(
-          "UniApp",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue,
+          title: const Text(
+            "UniApp",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
           ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-     // body: GridView.builder(gridDelegate: SliverFixedGridDelegate, itemBuilder: itemBuilder)
-    );
+        body: GridView.builder(
+            itemCount: 20,
+            gridDelegate:
+                const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemBuilder: (context, idx) {
+              return Container(
+                height: 100,
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.grey,
+                ),
+              );
+            }));
   }
 }
