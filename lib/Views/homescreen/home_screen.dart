e14f61f13c2479cd9bb7e9f570815768736c5d07
AdiByte30 <adityaandhale30@gmail.com>
2024-11-27 16:13:09 +0530

    video Player Added

--- lib/Views/homescreen/home_screen.dart
+++ lib/Views/homescreen/home_screen.dart
@@ -8,75 +8,110 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+  
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: [
-          const SliverAppBar(
-            stretchTriggerOffset: 200,
-            shadowColor: Color.fromARGB(255, 249, 66, 66),
-            // expandedHeight: 100,
-            shape: Border(
-              bottom: BorderSide(
-                  color: Color.fromARGB(255, 80, 191, 246), width: 5),
-            ),
-            floating: true,
-            stretch: true,
-            backgroundColor: Colors.blueGrey,
-            title: Text(
-              "Uni App",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            centerTitle: true,
-          ),
-          const SliverGap(35),
+          _buildSliverAppBar(),
+          const SliverToBoxAdapter(child: SizedBox(height: 35)),
           SliverPadding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             sliver: SliverGrid.builder(
-                itemCount: hometab.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 5 / 7,
-                  mainAxisSpacing: 20,
-                  crossAxisSpacing: 20,
-                ),
-                itemBuilder: (context, idx) {
-                  return Container(
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      color: Colors.white,
-                      boxShadow: const [
-                        BoxShadow(
-                            color: Color.fromARGB(255, 106, 106, 106),
-                            blurRadius: 15,
-                            spreadRadius: 0,
-                            offset: Offset(0, 6))
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                            height: 160,
-                            width: 160,
-                            child: Image.asset(hometab[idx].imagePath)),
-                        const Gap(24),
-                        Text(
-                          hometab[idx].name,
-                          style: GoogleFonts.roboto(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                            color: const Color.fromRGBO(25, 33, 38, 1),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                }),
+              itemCount: hometab.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 5 / 7,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+              ),
+              itemBuilder: (context, idx) {
+                return HomeTabCard(
+                  imagePath: hometab[idx].imagePath,
+                  name: hometab[idx].name,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSliverAppBar() {
+    return  SliverAppBar(
+      stretchTriggerOffset: 200,
+      shadowColor: const Color.fromARGB(255, 249, 66, 66),
+      shape: const Border(
+        bottom: BorderSide(
+          color: Color.fromARGB(255, 80, 191, 246),
+          width: 5,
+        ),
+      ),
+      floating: true,
+      stretch: true,
+      backgroundColor: Colors.blueGrey,
+      centerTitle: true,
+      title: Text(
+        "Uni App",
+        style: GoogleFonts.roboto(
+          fontSize: 24,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
+
+class HomeTabCard extends StatelessWidget {
+  final String imagePath;
+  final String name;
+
+
+  const HomeTabCard({
+    super.key,
+    required this.imagePath,
+    required this.name,
+  
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromARGB(255, 106, 106, 106),
+            blurRadius: 15,
+            spreadRadius: 0,
+            offset: Offset(0, 6),
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const Gap(16),
+          Text(
+            name,
+            style:const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              color:  Color.fromRGBO(25, 33, 38, 1),
+            ),
           ),
         ],
       ),
